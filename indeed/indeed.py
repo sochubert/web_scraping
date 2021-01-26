1eec3982ce0315fdf1fd9cfebf16c27dcfdea05d
Minjae Lee <sochubert@gmail.com>
2021-01-26 12:20:21 +0900

    start stackoverflow pages

--- indeed/indeed.py
+++ indeed/indeed.py
@@ -4,7 +4,7 @@ LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
 
@@ -46,4 +46,10 @@ def extract_indeed_jobs(last_page):
             job = extract_job(result)
             jobs.append(job)
             jobs.append("*******************")
+    return jobs
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_indeed_jobs(last_page)
     return jobs
